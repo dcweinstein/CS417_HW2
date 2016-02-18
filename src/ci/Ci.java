1869ea66a617ad620f60fc020a6c88655f41484c
dcweinstein <davidw1@iastate.edu>
2016-02-17 20:13:47 -0600

    first commit

---
+++ src/ci/Ci.java
@@ -0,0 +1,10 @@
+package ci;
+
+public class Ci {
+	public Ci() {
+		
+	}
+	public String hello_world() {
+		return "Hello World!";
+	}
+}
---
+++ src/ci/CiTest.java
@@ -0,0 +1,16 @@
+package ci;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CiTest {
+
+	@Test
+	public void test() {
+		Ci ci = new Ci();
+		String string = ci.hello_world();
+		assertTrue("string = 'Hello World!", string.equals("Hello World!"));
+	}
+
+}
