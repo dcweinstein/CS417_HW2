cbbcc4a7b6638c2463018317599944f2f54394f7
dcweinstein <davidw1@iastate.edu>
2016-02-17 20:25:52 -0600

    test change

--- src/ci/CiTest.java
+++ src/ci/CiTest.java
@@ -7,7 +7,8 @@ import org.junit.Test;
 public class CiTest {
 
 	@Test
-	public void test() {
+	public void test() 
+	{
 		Ci ci = new Ci();
 		String string = ci.hello_world();
 		assertTrue("string = 'Hello World!", string.equals("Hello World!"));
